16e646922deb334b1c1bf7975dfe1f3f081ee239
Alex D. Garcia <Alex D. Garcia>
2016-12-01 23:45:40 -0600

    Inital

---
+++ SecretSanta/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SecretSanta.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Next()
+        {
+            return "Carlos";
+        }
+
+        [HttpPost]
+        public string Received(string person)
+        {
+
+            return "true";
+        }
+    }
+}
