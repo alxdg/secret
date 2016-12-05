7e8333ac165733efe6b555ef8839a2530b37bd38
Alex Garcia <agarcia@gmrmarketing.com>
2016-12-05 09:41:57 -0600

    Add updating of database

--- SecretSanta/Controllers/HomeController.cs
+++ SecretSanta/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Runtime.Serialization;
+using System.Web.Mvc;
 
 namespace SecretSanta.Controllers
 {
@@ -11,16 +14,75 @@ namespace SecretSanta.Controllers
         }
 
         [HttpGet]
-        public string Next()
+        public JsonResult Next()
         {
-            return "Carlos";
+            string cnnString = ConfigurationManager.ConnectionStrings["InfocenterConnectionString"].ConnectionString;
+            Person person = new Person();
+            if (!String.IsNullOrWhiteSpace(cnnString))
+            {
+                using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(cnnString))
+                {
+                    connection.Open();
+
+                    var dbcommand = new System.Data.SqlClient.SqlCommand("SELECT TOP 1 * FROM SecretSanta WHERE ReceivedAGift = 0 ORDER BY NEWID()", connection);
+
+                    var dr = dbcommand.ExecuteReader();
+                    dr.Read();
+
+                    person.FullName = dr["DisplayName"].ToString();
+                    person.Department = dr["Department"].ToString();
+                    person.EmployeeId = dr["EmployeeID"].ToString();
+
+                    dr.Close();
+                    connection.Close();
+                }
+            }
+            return Json(new
+                        {
+                            person.FullName,
+                            person.Department,
+                            person.EmployeeId
+                        }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
-        public string Received(string person)
+        public JsonResult Received(string employeeId)
         {
+            try
+            {
+                string cnnString = ConfigurationManager.ConnectionStrings["InfocenterConnectionString"].ConnectionString;
+                Person person = new Person();
+                if (!String.IsNullOrWhiteSpace(cnnString))
+                {
+                    using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(cnnString))
+                    {
+                        connection.Open();
 
-            return "true";
+                        var dbcommand = new System.Data.SqlClient.SqlCommand($"UPDATE SecretSanta SET ReceivedAGift = 1 WHERE EmployeeID = '{employeeId}'", connection);
+
+                        dbcommand.ExecuteNonQuery();
+
+                        connection.Close();
+                    }
+                }
+                return Json(new {Error = false}, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json(new { Error = true }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [Serializable]
+        [DataContract(IsReference = true)]
+        public class Person
+        {
+            [DataMember]
+            public string FullName { get; set; }
+
+            public string Department { get; set; }
+
+            public string EmployeeId { get; set; }
         }
     }
 }
